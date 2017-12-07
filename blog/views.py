49900a14c0ed20a8e17fbf7dd8e84d1973fd3a3f
Natasha <natasha.pinto@ouh.nhs.uk>
2017-12-07 16:37:43 +0000

    adding a total box to check if sum=50

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Post 
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
 	return render(request, 'blog/post_list.html')
+
+	""" code for submit button
+
+	in post_list.html:
+		if sum of "No. of Cells"= 50
+		continue 
+		elif:
+			print "Error- A minimum of 50 cells need to be checked"
+
